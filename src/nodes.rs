1e1f84f158fcc2b1935375bf8e3308c13ba7e66f
Gustavo Bhering Grande <gustavobgrande@hotmail.com>
2025-06-12 16:05:03 -0300

    changed nodes to use methods instead of functions

--- src/nodes.rs
+++ src/nodes.rs
@@ -23,49 +23,51 @@ pub async fn get_nodes() -> Result<Vec<Value>, Error> {
     let response = reqwest::get(FETCH_URL).await?.json::<Vec<serde_json::Value>>().await?;
     return Ok(response);
 }
+impl Node {
+    //receives json and converts it to Node struct
+    pub fn build_from_json(json : &Value) -> Node {
+        let capacity_in_sats = json["capacity"].as_i64();
+        let capacity_in_btc : f64;
+        //if for any reason any of the fields is, it will be replace by a default value - empty string or epoch as a date
+        match capacity_in_sats {
+            Some(num) => capacity_in_btc = num as f64/BTC_SATS_RATIO as f64,
+            None => capacity_in_btc = 0f64
+        }
+        let first_seen_in_seconds : i64 = match json["firstSeen"].as_i64() {
+            Some(t) => t ,
+            None => 0
+        };
+        let first_seen_as_date :  DateTime<Utc> =  match DateTime::from_timestamp(first_seen_in_seconds, 0) {
+            Some(date) => date,
+            None => DateTime::from_timestamp(0, 0).unwrap()
+        };
+        let alias : String = match json["alias"].as_str() {
+            Some(string) => string.to_string(),
+            None => String::new()
 
-//receives json and converts it to Node struct
-pub fn build_node(json : &Value) -> Node {
-    let capacity_in_sats = json["capacity"].as_i64();
-    let capacity_in_btc : f64;
-    //if for any reason any of the fields is, it will be replace by a default value - empty string or epoch as a date
-    match capacity_in_sats {
-        Some(num) => capacity_in_btc = num as f64/BTC_SATS_RATIO as f64,
-        None => capacity_in_btc = 0f64
+        };
+        let public_key : String = match json["publicKey"].as_str() {
+            Some(string) => string.to_string(),
+            None => String::new()
+
+        };
+        let new_node = Node {
+            capacity : capacity_in_btc,
+            public_key : public_key,
+            alias : alias,
+            first_seen : first_seen_as_date
+        };
+        return new_node;
     }
-    let first_seen_in_seconds : i64 = match json["firstSeen"].as_i64() {
-        Some(t) => t ,
-        None => 0
-    };
-    let first_seen_as_date :  DateTime<Utc> =  match DateTime::from_timestamp(first_seen_in_seconds, 0) {
-        Some(date) => date,
-        None => DateTime::from_timestamp(0, 0).unwrap()
-    };
-    let alias : String = match json["alias"].as_str() {
-        Some(string) => string.to_string(),
-        None => String::new()
 
-    };
-    let public_key : String = match json["publicKey"].as_str() {
-        Some(string) => string.to_string(),
-        None => String::new()
+    //converts a json Vector to a node struct Vector
+    pub fn build_nodes_vec(vec_json : Vec<Value>) -> Vec<Node> {
+        let mut node_vec : Vec<Node> = Vec::new();
+        for json in vec_json.iter() {
+            node_vec.push(Node::build_from_json(json));
+        }
+        return node_vec;
 
-    };
-    let new_node = Node {
-        capacity : capacity_in_btc,
-        public_key : public_key,
-        alias : alias,
-        first_seen : first_seen_as_date
-    };
-    return new_node;
-}
-
-//converts a json Vector to a node struct Vector
-pub fn build_nodes_vec(vec_json : Vec<Value>) -> Vec<Node> {
-    let mut node_vec : Vec<Node> = Vec::new();
-    for json in vec_json.iter() {
-        node_vec.push(build_node(json));
     }
-    return node_vec;
 
 }
