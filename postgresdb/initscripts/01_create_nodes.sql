56da8a5d4b43c3e05a703dbc5a97a32a59fcfc0f
Gustavo Bhering Grande <gustavobgrande@hotmail.com>
2025-06-12 11:59:22 -0300

    fix create table querry

--- postgresdb/initscripts/01_create_nodes.sql
+++ postgresdb/initscripts/01_create_nodes.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS nodes;
-CREATE TABLE nodes {
+CREATE TABLE nodes (
     id SERIAL PRIMARY KEY,
     public_key VARCHAR(66) NOT NULL,
     alias VARCHAR(256) NOT NULL,
     capacity FLOAT NOT NULL,
-    first_seen DateTime<Utc>
-}
+    first_seen TIMESTAMP WITH TIME ZONE
+);
