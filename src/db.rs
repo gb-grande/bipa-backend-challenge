1f5a2a84a3961f54570b054974d9e1f003c4a5df
Gustavo Bhering Grande <gustavobgrande@hotmail.com>
2025-06-13 15:55:48 -0300

    reconnect if db connection is lost and add request error handling

--- src/db.rs
+++ src/db.rs
@@ -1,22 +1,37 @@
 use tokio_postgres::{
-    Client, Error, NoTls
+    Client, NoTls
 };
-use std::time::Duration;
+use std::{time::Duration};
 use crate::nodes;
 use crate::nodes::Node;
 use crate::query_builder;
+use std::sync::{Arc};
+use tokio::sync::Mutex;
+use serde_json::Value;
 //tries to connect to db client, 
-pub async fn try_to_connect(conn_string : &str) -> Client{
+
+pub struct AppState {
+    pub client: Arc<Mutex<Option<Client>>>
+}
+
+
+pub async fn try_to_connect(conn_string : String, shared_state : Arc<AppState>) -> (){
+    //reconection loop
     loop {
-        match tokio_postgres::connect(conn_string, NoTls).await{
+        match tokio_postgres::connect(conn_string.as_str(), NoTls).await{
             Ok((client, connection)) =>{
                 tokio::spawn(async move {
                     if let Err(e) = connection.await {
                         eprintln!("connection error: {}", e);
                     }
+
                 });
-                return client;
-            }
+                //update app state with client
+                *shared_state.client.lock().await = Some(client);
+                println!("Connection with db estabilished");
+                return ;
+                }
+
             Err(e) => {
                 println!("Error {}", e);
                 println!("Connection failed, retrying in 5 seconds");
@@ -26,20 +41,36 @@ pub async fn try_to_connect(conn_string : &str) -> Client{
     }    
 }
 
-//update nodes table
-pub async fn update_nodes(client : &Client) -> Result<(), Box<dyn std::error::Error>>{
+//update nodes table upon receiving appstate
+//only lock after fetching nodes
+pub async fn update_nodes(appstate : Arc<AppState>) -> Result<(), Box<dyn std::error::Error>>{
     println!("Updating nodes");
+    let json_vec : Vec<Value>;
     //fetches from api
-    let json_vec = nodes::get_nodes().await?;
+    loop {
+        match nodes::get_nodes().await {
+            Ok(vec) => {
+                json_vec = vec;
+                break;
+            }
+            Err(e) =>{
+                println!("Error {} while trying to fetch, will retry in 10s", e.to_string());
+                tokio::time::sleep(Duration::from_secs(10)).await;
+            }
+ 
+        };
+
+    }
     //converts to node struct
     let nodes_vec = nodes::Node::build_nodes_vec_from_json(json_vec);
     //builds query
     let query = query_builder::UpdateDbQuery::build_from_nodes(&nodes_vec);
     let query_str = query.get_querry();
-    client.simple_query(query_str).await?;
+    appstate.client.lock().await.as_ref().unwrap().simple_query(query_str).await?;
     println!("Succesful update");
     return Ok(());
 }
+
 
 pub async fn get_nodes_in_db(client : &Client) -> Result<Vec<Node>, Box<dyn std::error::Error>>{
     let rows = client.query("SELECT * FROM nodes;", &[]).await?;
@@ -48,11 +79,3 @@ pub async fn get_nodes_in_db(client : &Client) -> Result<Vec<Node>, Box<dyn std:
     
 }
 
-
-// pub async fn is_nodes_table_empty(client : &Client) -> Result<bool, Error>{
-//     let rows = client.query("SELECT * FROM NODES;", &[]).await?;
-//     if rows.len() == 0 {
-//         return Ok(true);
-//     }
-//     return Ok(false);
-// }
--- src/main.rs
+++ src/main.rs
@@ -3,63 +3,67 @@ mod db;
 use std::{sync::Arc, time::Duration};
 mod query_builder;
 use axum::{
-    extract::State,
-    routing::get,
-    response::Json,
-    Router
+    extract::State, response::Json, routing::get,  Router
 };
 use serde_json::{Value, json};
-use tokio_postgres::{
-    Client
-};
-
-use crate::db::update_nodes;
-struct AppState {
-    client: Client
-
-}
-
+use tokio::sync::Mutex;
+use crate::db::{try_to_connect, update_nodes};
+use db::AppState;
 const PORT : &str = "3000";
 const CONN_STRING : &str = "host=localhost user=admin password=admin123 dbname=nodesdb port=5000";
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
-    let client = db::try_to_connect(CONN_STRING).await;
-    let shared_state = Arc::new(AppState { client : client});
+    let shared_state = Arc::new(AppState {client : Arc::new(Mutex::new(None))});
+    db::try_to_connect(CONN_STRING.to_owned(), shared_state.clone()).await;
     println!("Fetching nodes on server startup");
-    update_nodes(&shared_state.client).await?;
+    {
+        //it's 
+        let _ = update_nodes(shared_state.clone()).await;
+    }
     let st_clone = shared_state.clone();
     tokio::spawn(async move {
-        update_every_duration(&st_clone.client, Duration::from_secs(60)).await;
+        match update_every_duration(st_clone.clone(), Duration::from_secs(60)).await {
+            Ok(_) => (),
+            Err(error ) => {
+                eprintln!("Failed to update, error: {}", error.to_string());
+                tokio::spawn(try_to_connect(CONN_STRING.to_owned(), st_clone.clone()));
+            }
+
+        };
     });
-    let app : Router<()> = Router::new().route("/nodes", get(serve_nodes_json)).with_state(shared_state);
+    let st_clone = shared_state.clone();
+    let app : Router<()> = Router::new().route("/nodes", get(serve_nodes_json).with_state(st_clone));
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{PORT}")).await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app).await.unwrap();  
 
 
     return Ok(());
     
 
 }
-
 async fn serve_nodes_json (State(state): State<Arc<AppState>>) -> Json<Value> {
-    match db::get_nodes_in_db(&state.client).await {
+    let guard = state.client.lock().await;
+    let client = guard.as_ref().unwrap();
+    match db::get_nodes_in_db(client).await {
         Ok(nodes_vec) => return Json(json!(nodes_vec)),
-        Err(e) => return Json(json!({
-            "code" : 500,
-            "error" : e.to_string()
-
-        }))
+        Err(e) => {
+            //tries to connect again
+            tokio::spawn(try_to_connect(CONN_STRING.to_owned(), state.clone()));
+            return Json(json!({
+                "code" : 500,
+                "error" : e.to_string()
+            }))
+        } 
     }
  }
 
-async fn update_every_duration(client : &Client, duration : Duration) -> Result<(), Box<dyn std::error::Error>>{
+async fn update_every_duration(shared_state : Arc<AppState>, duration : Duration) -> Result<(), Box<dyn std::error::Error>>{
     let mut interval = tokio::time::interval(duration);
     //first tick so it waits the duration before ticking again
     interval.tick().await;
     loop {
         interval.tick().await;
-        update_nodes(client).await?;
+        update_nodes(shared_state.clone()).await?;
     }
 }
 
